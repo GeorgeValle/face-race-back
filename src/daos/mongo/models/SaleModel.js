035e512356e79a8b70f9be3f21b28d5b64cc5cf0
GeorgeValle <jorgeguillermovalle@gmail.com>
2025-04-14 03:01:43 -0300

    (fix): change autoincrement function in saleModel.js

--- src/daos/mongo/models/SaleModel.js
+++ src/daos/mongo/models/SaleModel.js
@@ -1,13 +1,17 @@
 import { Schema, model } from 'mongoose';
+import mongoose from 'mongoose';
 
-import sequence from 'mongoose-sequence';
+import mongooseCounter from 'mongoose-counters';
+
+
+const counter = mongooseCounter(mongoose);
 
 const saleSchema = new Schema({
 
     saleNumber: {
         type: Number,
         index: true,
-        unique: true,
+        
     },
     payment: {
         type: Array,
@@ -43,22 +47,6 @@ const saleSchema = new Schema({
 },
     { timestamps: true })
 
-const sequencePlugin = sequence(mongoose);
-saleSchema.plugin(sequencePlugin, {
-    field: 'numberSale',
-    startAt: 4012,
-    incrementBy: 1,
-    id: 'sale_number'
-});
-
-
-
-sequencePlugin.createSequence('sale_number', (err, sequence) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Secuencia creada');
-    }
-});
+    saleSchema.plugin(counter, { Field: 'saleNumber' });
 
 export default model('sale', saleSchema);
